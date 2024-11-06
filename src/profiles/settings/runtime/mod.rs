c90846714a297d4ec3c2c1aa4d7f8c22dccfd496
Nikita Podvirnyi <krypt0nn@vk.com>
2024-11-06 16:11:21 +0200

    feat(profiles): remove pointless native runtimes settings

--- src/profiles/settings/runtime/mod.rs
+++ src/profiles/settings/runtime/mod.rs
@@ -2,13 +2,9 @@ use serde_json::Value as Json;
 
 use crate::prelude::*;
 
-mod windows_native;
-mod linux_native;
 mod linux_wine;
 
 pub mod prelude {
-    pub use super::windows_native::Settings as WindowsNativeProfileRuntimeSettings;
-    pub use super::linux_native::Settings as LinuxNativeProfileRuntimeSettings;
     pub use super::linux_wine::Settings as LinuxWineProfileRuntimeSettings;
 
     pub use super::Runtime as RuntimeProfileSettings;
@@ -16,42 +12,25 @@ pub mod prelude {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Runtime {
-    WindowsNative(windows_native::Settings),
-    LinuxNative(linux_native::Settings),
-    LinuxWine(linux_wine::Settings)
+    LinuxWine(linux_wine::Settings),
+    None
 }
 
 impl Runtime {
     pub fn to_json(&self) -> Result<Json, AsJsonError> {
         match self {
-            Self::WindowsNative(settings) => settings.to_json(),
-            Self::LinuxNative(settings) => settings.to_json(),
-            Self::LinuxWine(settings) => settings.to_json()
+            Self::LinuxWine(settings) => settings.to_json(),
+            Self::None => Ok(Json::Null)
         }
     }
 
     pub fn from_json(target_platform: &TargetPlatform, json: &Json) -> Result<Self, AsJsonError> {
         match target_platform {
-            TargetPlatform::X86_64_windows_native => Ok(Self::WindowsNative(windows_native::Settings::from_json(json)?)),
-            TargetPlatform::X86_64_linux_native => Ok(Self::LinuxNative(linux_native::Settings::from_json(json)?)),
-
             TargetPlatform::X86_64_linux_wine32 |
-            TargetPlatform::X86_64_linux_wine64 => Ok(Self::LinuxWine(linux_wine::Settings::from_json(json)?))
+            TargetPlatform::X86_64_linux_wine64 => Ok(Self::LinuxWine(linux_wine::Settings::from_json(json)?)),
+
+            _ => Ok(Self::None)
         }
-    }
-}
-
-impl From<windows_native::Settings> for Runtime {
-    #[inline]
-    fn from(value: windows_native::Settings) -> Self {
-        Self::WindowsNative(value)
-    }
-}
-
-impl From<linux_native::Settings> for Runtime {
-    #[inline]
-    fn from(value: linux_native::Settings) -> Self {
-        Self::LinuxNative(value)
     }
 }
 
